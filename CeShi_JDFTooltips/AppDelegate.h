79ffbb5f8bf56746034257f4ca7224d1689be46a
fushengjun <fushengjun@xiangshang360.com>
2016-04-22 15:36:39 +0800

    Initial Commit

---
+++ CeShi_JDFTooltips/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CeShi_JDFTooltips
+//
+//  Created by SPIREJ on 16/4/22.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CeShi_JDFTooltips/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CeShi_JDFTooltips
+//
+//  Created by SPIREJ on 16/4/22.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
